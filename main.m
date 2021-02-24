14294dcd17edf076fdd836355256010735f8e0df
sho <sho@DESKTOP-1D4KHED>
2021-02-23 23:00:12 -0500

    First commit with some changes on simulink model step.slx

--- main.m
+++ main.m
@@ -2,3 +2,8 @@ addpath('func/');
 addpath('src');
 addpath('ui');
 
+const=[6.1,...       % Length   [m]
+       1.83];           % Diameter [m]
+   
+qci = 0.0711;        % Oil crude inflow [m3/s] -> 38648 BPD
+qwi = 0.1374;        % Water inflow [m3/s] -> 74711 BPD
