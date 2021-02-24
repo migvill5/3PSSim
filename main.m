dc727e018e28beb7d7108ca15509b25a4669e2ae
sho <sho@DESKTOP-1D4KHED>
2021-02-23 23:23:29 -0500

    Modification of conditions

--- main.m
+++ main.m
@@ -2,8 +2,10 @@ addpath('func/');
 addpath('src');
 addpath('ui');
 
-const=[6.1,...       % Length   [m]
-       1.83];           % Diameter [m]
+const=[6.1,...       % Length A  [m]
+       1.83,...      % Diameter  [m]
+       1.22,...      % Length B  [m]
+       1.00];        % Length hs [m]
    
 qci = 0.0711;        % Oil crude inflow [m3/s] -> 38648 BPD
 qwi = 0.1374;        % Water inflow [m3/s] -> 74711 BPD
